4a5837ef5cf7e93149813d5e5900823ad00081fb
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-09-08 23:59:34 +0900

    [main] redis 관련 파일 수정

--- src/main/java/com/couponevent/board/BoardService.java
+++ src/main/java/com/couponevent/board/BoardService.java
@@ -1,5 +1,6 @@
 package com.couponevent.board;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,12 +10,15 @@ import java.util.List;
 
 @Service
 public class BoardService {
+
     private BoardRepository boardRepository;
+    private Pageable pageable;
 
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
