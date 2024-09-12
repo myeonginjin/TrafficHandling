fa2c19ded2de9103335bcafe40aa114851d34e30
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-09-12 10:49:48 +0900

    [main] 테스트 코드 작성

--- src/main/java/com/couponevent/CouponEventApplication.java
+++ src/main/java/com/couponevent/CouponEventApplication.java
@@ -2,7 +2,9 @@ package com.couponevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CouponEventApplication {
 
