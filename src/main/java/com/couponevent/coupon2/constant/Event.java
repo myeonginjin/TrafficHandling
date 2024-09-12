3bb418b2123287600ce321e7fae2a79c13a15ed6
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-09-12 21:45:15 +0900

    [main] 공부해야할 내용 주석 추가

--- src/main/java/com/couponevent/coupon2/constant/Event.java
+++ src/main/java/com/couponevent/coupon2/constant/Event.java
@@ -1,14 +1,14 @@
 package com.couponevent.coupon2.constant;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum Event {
-    CHICKEN("치킨");
+    CHICKEN("치킨"),
+    PIZZA("피자"),
+    COFFEE("커피");
 
     private String name;
-
-    Event(String name) {
-        this.name = name;
-    }
 }
